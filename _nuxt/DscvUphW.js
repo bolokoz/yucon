9897698c12e859a5f515d58d67309a5cb1cb6b56
bolokoz <yurifbeck@gmail.com>
2025-09-01 12:36:28 +0000

    Deploying to gh-pages from @ bolokoz/yucon@5057018909fb4166eabd244ed48199bc25e42d3f 🚀

---
+++ _nuxt/DscvUphW.js
@@ -0,0 +1,1 @@
+import{r as s}from"./rMGil2UT.js";import t from"./BTh6cUrP.js";import{z as e,H as a,i as p,w as c,W as i}from"./CjIrpI3r.js";import"./BN_7HF1G.js";const d={__name:"ProseTip",setup(n){const o=e();return(r,f)=>(p(),a(t,{color:"success",icon:i(o).ui.icons.tip},{default:c(()=>[s(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
